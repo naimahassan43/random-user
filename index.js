8da6035126a044f9fccdebfc04c8ceb7a9451813
Naima Hassan <naima.hassan434@gmail.com>
2021-10-29 00:19:09 +0100

    Show users functions

--- index.js
+++ index.js
@@ -9,5 +9,46 @@ const getData = async () => {
 };
 getData().then((data) => {
   let users = data.results;
-  console.log(users);
+  // console.log(users);
+  showUsers(users);
 });
+
+//Show Users functions
+
+function showUsers(users) {
+  users.forEach((user) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.className = "card";
+
+    const cardImgDiv = document.createElement("div");
+    cardImgDiv.className = "card-img-container";
+
+    const cardImg = document.createElement("img");
+    cardImg.className = "card-img";
+    cardImg.src = `${user.picture.medium}`;
+    cardImg.alt = `${user.name.first} ${user.name.last}`;
+
+    const cardInfoDiv = document.createElement("div");
+    cardInfoDiv.className = "card-info-container";
+
+    const name = document.createElement("h3");
+    name.className = "card-name";
+    name.id = "name";
+    name.innerText = `${user.name.first} ${user.name.last}`;
+
+    const emailP = document.createElement("p");
+    emailP.className = "card-text";
+    emailP.innerText = `${user.email}`;
+    const locationP = document.createElement("p");
+    locationP.className = "card-text cap";
+    locationP.innerText = `${user.location.city}, ${user.location.state}`;
+
+    // Append Child
+    cardImgDiv.append(cardImg, cardInfoDiv);
+    cardInfoDiv.append(name, emailP, locationP);
+
+    cardDiv.append(cardImgDiv, cardInfoDiv);
+
+    gallery.append(cardDiv);
+  });
+}
