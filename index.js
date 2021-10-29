5611a4ff1af5b04b2404b74c1ef278c1ccf26bf9
Naima Hassan <naima.hassan434@gmail.com>
2021-10-29 18:22:35 +0100

    Modal function

--- index.js
+++ index.js
@@ -1,49 +1,40 @@
 const gallery = document.querySelector("#gallery");
 const searchContainer = document.querySelector(".search-container");
-//fetch data from server
 
+//fetch data from server
 const getData = async () => {
   const res = await fetch(`https://randomuser.me/api/?results=12`);
-
   return await res.json();
 };
 getData().then((data) => {
   let users = data.results;
-  console.log(users);
   showUsers(users);
   showModalUser(users);
 });
 
 //Show Users functions
-
 function showUsers(users) {
   users.forEach((user) => {
     const cardDiv = document.createElement("div");
     cardDiv.className = "card";
-
     const cardImgDiv = document.createElement("div");
     cardImgDiv.className = "card-img-container";
-
     const cardImg = document.createElement("img");
     cardImg.className = "card-img";
     cardImg.src = `${user.picture.medium}`;
     cardImg.alt = `${user.name.first} ${user.name.last}`;
-
     const cardInfoDiv = document.createElement("div");
     cardInfoDiv.className = "card-info-container";
-
     const name = document.createElement("h3");
     name.className = "card-name";
     name.id = "name";
     name.innerText = `${user.name.first} ${user.name.last}`;
-
     const emailP = document.createElement("p");
     emailP.className = "card-text";
     emailP.innerText = `${user.email}`;
     const locationP = document.createElement("p");
     locationP.className = "card-text cap";
     locationP.innerText = `${user.location.city}, ${user.location.state}`;
-
     // Append Child
     cardImgDiv.append(cardImg, cardInfoDiv);
     cardInfoDiv.append(name, emailP, locationP);
@@ -90,8 +81,6 @@ searchContainer.addEventListener("keyup", function (e) {
       cardUser.style.display = "none";
     }
   }
-
-  // console.log(keyword);
 });
 
 document.querySelector("#form-search").addEventListener("submit", (e) => {
@@ -143,10 +132,11 @@ function showModalUser(users) {
     const modalAdrsP = document.createElement("p");
     modalAdrsP.className = "modal-text";
     modalAdrsP.innerText = `${user.location.street.number} ${user.location.street.name}, ${user.location.city}, OR ${user.location.postcode}`;
-    // const modalBirthP = document.createElement("p");
-    // modalBirthP.className = "modal-text";
-    // modalBirthP.innerText = `${user.dob.date}`;
+
     //Append child
+    modalContDiv.append(modalDiv);
+    modalDiv.append(closeBtn, modalInfoDiv);
+    closeBtn.append(strong);
     modalInfoDiv.append(
       modalImg,
       modalName,
@@ -156,9 +146,22 @@ function showModalUser(users) {
       modalPhnP,
       modalAdrsP
     );
-    closeBtn.append(strong);
-    modalDiv.append(closeBtn, modalInfoDiv);
-    modalContDiv.append(modalDiv);
-    console.log(modalContDiv);
   });
 }
+
+// Event listeners
+
+// const previewCards = document.querySelector(".gallery");
+// console.log(previewCards);
+// const modal = document.querySelector(".modal");
+// const close = document.querySelector(".modal-close-btn");
+
+// previewCards.forEach((previewCard) => {
+//   previewCard.addEventListener("click", () => {
+//     modal.classList.add("open");
+//   });
+// });
+
+// close.addEventListener("click", () => {
+//   close.classList.remove("open");
+// });
